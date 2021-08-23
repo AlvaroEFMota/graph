4367be18cfaffb8d5e20260a606f45e4a9952cbc
alvaro <alvaro11318@gmail.com>
2021-08-22 23:26:54 -0300

    primeiro commit

---
+++ target/debug/deps/graph-d96cdf6f59ec75da.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/alvar/graph/target/debug/deps/graph-d96cdf6f59ec75da: src/main.rs
+
+/mnt/c/Users/alvar/graph/target/debug/deps/graph-d96cdf6f59ec75da.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/graph.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/alvar/graph/target/debug/graph: /mnt/c/Users/alvar/graph/src/main.rs
